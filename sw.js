ae52ce08785cfc9f6878fdf1d1205ea20914deaa
databasebiazotti-code <database.biazotti@gmail.com>
2026-05-23 13:25:01 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
-const CACHE_NAME = 'catalogo-iphones-v1';
-const CORE = ['./', './index.html', './manifest.json',
-  './assets/images/pack-artes-canva.png',
-  './assets/images/pack-videos-virais.png',
-  './assets/images/lista-fornecedores.png',
-  './assets/images/planilha-precificacao.png',
-  './assets/images/contrato-garantia.png',
-  './assets/images/pack-documentos-assistencia.png'
+const CACHE_NAME = 'catalogo-iphones-flat-v1';
+const CORE = [
+  './', './index.html', './manifest.json',
+  './pack-artes-canva.png', './pack-videos-virais.png',
+  './lista-fornecedores.png', './planilha-precificacao.png',
+  './contrato-garantia.png', './pack-documentos-assistencia.png'
 ];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)));
   self.skipWaiting();
 });
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  event.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+  )));
   self.clients.claim();
 });
 self.addEventListener('fetch', event => {
